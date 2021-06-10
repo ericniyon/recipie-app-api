596c5736c899eb27b706123a96600cc38e43a7d7
Eric <niyoeri6@gmail.com>
2021-06-10 11:37:53 +0200

    lit all users on admin site

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,23 +4,23 @@ from django.urls import reverse
 
 class AdminSiteTests(TestCase):
 
- def setUp(self):
-  self.client = Client()
-  self.admin_user = get_user_model().objects.create_superuser(
-   email = 'admin@gmail.com',
-   password = 'Test123'
-  )
-  self.client.force_login(self.admin_user)
-  self.user = get_user_model().objects.create_user(
-   email = 'test@go.com',
-   password = 'Test123',
-   name = 'test user name'
-  )
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@gmail.com',
+            password = 'Test123'
+            )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+        email = 'test@go.com',
+        password = 'Test123',
+        name = 'test user name'
+        )
 
- def test_users_listed(self):
-  """ chacking if user are listed """
-  url = reverse('admin:core_user_changelist')
-  res = self.client.get(url)
+    def test_users_listed(self):
+        """ chacking if user are listed """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-  self.assertContains(res, self.user.name)
-  self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,19 +3,20 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTest(TestCase):
-  
+
   def test_create_user_with_valid_email(self):
     email = 'niyo@gmail.com'
     password = 'Test123'
 
     user = get_user_model().objects.create_user(
-   email = email,
-   password = password
+    email = email,
+    password = password
   )
     self.assertEqual(user.email, email)
     self.assertTrue(user.check_password(password))
 
   def test_new_user_email_normarized(self):
+    
     """ testing user email is lower"""
     email = 'niyo@GMAIL.com'
     user = get_user_model().objects.create_user(email, 'Test123')
