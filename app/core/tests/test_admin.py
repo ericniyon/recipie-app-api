15a88eb3eb9c765e396e13910e7fdcc2c9f335b7
Eric <niyoeri6@gmail.com>
2021-06-10 11:45:46 +0200

    lit all users on admin site

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,19 +2,20 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@gmail.com',
-            password = 'Test123'
-            )
+            email='admin@gmail.com',
+            password='Test123'
+        )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-        email = 'test@go.com',
-        password = 'Test123',
-        name = 'test user name'
+            email='test@go.com',
+            password='Test123',
+            name='test user name'
         )
 
     def test_users_listed(self):
